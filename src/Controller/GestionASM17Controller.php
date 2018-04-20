83d7d72c77c9fea6f1fc176bad85f8ee0139e49d
Kourylles <38153882+Kourylles@users.noreply.github.com>
2018-04-20 16:45:57 +0200

    Page Accueil V1

---
+++ src/Controller/GestionASM17Controller.php
@@ -0,0 +1,37 @@
+<?php
+// GestionASM17/src/Controller/GestionASM17Controller.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class GestionASM17Controller extends Controller
+{
+    public function accueil()
+    {
+        $NbreMembre = "BDD";
+        $NbreMembreOkCoti = "BDD";
+        $NbreMembreNokCoti ="Calcul";
+        $NbreDonateur ="BDD";
+        $TotalRecette ="BDD";
+        $TotalCoti ="BDD";
+        $TotalDon="BDD";
+        $TotalRecette="BDD";
+        $TotalSubvention="BDD";
+        $TotalDepense="BDD";
+
+        return $this->render('GestionASM17/accueil.html.twig', array(
+            'NbreMembre'=>$NbreMembre,
+            'NbreMembreOkCoti'=>$NbreMembreOkCoti,
+            'NbreMembreNokCoti'=>$NbreMembreOkCoti,
+            'NbreDonateur'=>$NbreDonateur,
+            'TotalRecette'=>$TotalRecette,
+            'TotalCoti'=>$TotalCoti,
+            'TotalDon'=>$TotalDon,
+            'TotalRecette'=>$TotalRecette,
+            'TotalSubvention'=>$TotalSubvention,
+            'TotalDepense'=>$TotalDepense
+
+        ));
+    }
+}
